fe23f9cd8831bf3d7bfcc08159f7961795dd8002
Tejashwini Nityanand Godyal <tejashwinigodyal@gmail.com>
2025-12-18 14:21:34 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  email: { type: String, unique: true },
+  password: String,
+  balance: { type: Number, default: 0 }, // ✅ wallet
 });
 
 module.exports = mongoose.model("User", UserSchema);
